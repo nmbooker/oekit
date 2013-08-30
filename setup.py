edbbc4aa09dade671a0318165de314d358bfd6b0
Nick Booker <nick.booker@opusvl.com>
2013-08-30 12:11:46 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name = 'oekit',
-    version = '0.3',
+    version = '0.4',
     packages = setuptools.find_packages(),
     scripts = ['oe_dump_csv', 'oe_describe'],
 )
