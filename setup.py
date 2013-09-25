ed2947e425d4c17dac5b05c27bf6ac4f6ab1372a
Nick Booker <nick.booker@opusvl.com>
2013-09-25 14:53:13 +0100

    Bump version to 0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name = 'oekit',
-    version = '0.5',
+    version = '0.6',
     packages = setuptools.find_packages(),
     scripts = ['oe_dump_csv', 'oe_describe'],
 )
