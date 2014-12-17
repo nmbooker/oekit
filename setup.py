c81860fdecc97be21373ca6fcbb8b671e5f13e78
Nick Booker <nick.booker@opusvl.com>
2014-12-17 13:14:02 +0000

    Release v0.13:
    * Added dependency erppeek, as now mandatory for scripts

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name = 'oekit',
-    version = '0.12',
+    version = '0.13',
     packages = setuptools.find_packages(),
     install_requires = ['erppeek>=1.6'],
     scripts = [
