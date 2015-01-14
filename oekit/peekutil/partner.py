3df3b80cc87377caa3e07711dbb9f273c691e5e8
Nick Booker <nick.booker@opusvl.com>
2015-01-14 16:39:31 +0000

    Added get_title_id_map function.

---
+++ oekit/peekutil/partner.py
@@ -0,0 +1,30 @@
+
+"""erppeek helper for doing common queries on partner.
+"""
+
+def get_title_id_map(peek):
+    """Get mapping {title_string: title_id} from erppeek client object peek.
+    """
+    titles = peek.model('res.partner.title').browse([])
+    return {t.name: t.id for t in titles}
+
+__COPYRIGHT__ = """
+
+This program is part of oekit: https://github.com/nmbooker/oekit
+
+Copyright (c) 2015 Nicholas Booker <NMBooker@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
