5b52b1b0ef6d591c4c2fc5b8ebeb7e1a1b7dbd31
Nick Booker <NMBooker@gmail.com>
2016-05-29 17:02:57 +0100

    Test config chaining works OK

---
+++ tests/config/test_chain.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""Test chaining of configs.
+"""
+
+from oekit.config.chained import OEConfigChain
+
+conf_global = {
+    'url': 'http://localhost:8069',
+    'dbname': 'odoo',
+}
+
+conf_local = {
+    'dbname': 'test',
+    'user': 'admin',
+    'password': 'topsecret',
+}
+
+chain = OEConfigChain(configs=[conf_local, conf_global])
+
+def test_fall_through_global():
+    assert chain.get('url') == 'http://localhost:8069'
+
+
+def test_local_only():
+    assert chain.get('user') == 'admin'
+    assert chain.get('password') == 'topsecret'
+
+
+def test_local_trumps_global():
+    assert chain.get('dbname') == 'test'
